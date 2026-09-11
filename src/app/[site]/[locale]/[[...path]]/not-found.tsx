adae1e465820c945c2bd29e4ce078ec547600f56
RELEVANCELAB\RLE1639 <dalwa8qy@daiichisankyo.co.jp>
2026-09-12 01:28:39 +0530

    fixed npm run lint command issues

--- src/app/[site]/[locale]/[[...path]]/not-found.tsx
+++ src/app/[site]/[locale]/[[...path]]/not-found.tsx
@@ -9,23 +9,25 @@ import { NextIntlClientProvider } from "next-intl";
 export default async function NotFound() {
   const { site, locale } = getCachedPageParams();
 
+  let page;
+
   try {
-    const page = await client.getErrorPage(ErrorPage.NotFound, {
+    page = await client.getErrorPage(ErrorPage.NotFound, {
       site: site || scConfig.defaultSite,
       locale: locale || scConfig.defaultLanguage,
     });
-
-    if (page) {
-      return (
-        <NextIntlClientProvider>
-          <Providers page={page}>
-            <Layout page={page} />
-          </Providers>
-        </NextIntlClientProvider>
-      );
-    }
   } catch (error) {
     console.error("Error fetching 404 page:", error);
+  }
+
+  if (page) {
+    return (
+      <NextIntlClientProvider>
+        <Providers page={page}>
+          <Layout page={page} />
+        </Providers>
+      </NextIntlClientProvider>
+    );
   }
 
   return (
